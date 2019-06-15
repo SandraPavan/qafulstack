7445a16d523599ffe96f58e7b960de44aceb2769
Sandra Maira Pavan <sandra.pavan@institutosoma.org.br>
2019-06-15 08:59:36 -0300

    inicial

---
+++ ruby/metodos.rb
@@ -0,0 +1,21 @@
+def diga_ola(nome)
+    puts 'Olá! ' + nome
+end
+
+diga_ola('Maira')
+
+def retorna_nome
+    'Sandra'
+end
+
+resultado = retorna_nome
+puts resultado
+
+def soma(v1, v2)
+    total = v1 + v2
+    total
+end
+
+res = soma(10,25)
+puts res
+
